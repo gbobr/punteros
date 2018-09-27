9d9efb4decd34671bfcb1f4c512c0788c2816d31
CyanideBoyy <43642515+CyanideBoyy@users.noreply.github.com>
2018-09-27 09:26:43 -0300

    Tomas Gonzalez 6°C

--- ejercicio1/intercambio.c
+++ ejercicio1/intercambio.c
@@ -1,3 +1,18 @@
 #include "intercambio.h"
 
 //Escriba aquí su código!
+void intercambio(int v1, int v2){
+    int aux;
+    aux = *v1;
+    *v1 = *v2;
+    *v2 = aux;
+}
+void ordenarEnteros(int Enteros[], int Cant);
+    int ordenar = 0, i;
+    while (ordenar == 0){
+        for(i=0; i< Cant; i++)
+            ordenar = 1;
+            if(Enteros[i] > Enteros[i+1])
+                ordenar = 0;
+            intercambio(Enteros[i], Enteros[i+1]);
+    }
