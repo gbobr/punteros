c904eb1536cff2ceb4e5281dddc9c0682cf6ce98
matingb <43641235+matingb@users.noreply.github.com>
2018-09-27 08:41:37 -0300

    Resolucion del ejercicio (Matias Garcia)

--- ejercicio1/intercambio.c
+++ ejercicio1/intercambio.c
@@ -1,3 +1,28 @@
 #include "intercambio.h"
 
-//Escriba aquí su código!
+
+void intercambio(int* pi1, int* pi2)
+{
+    int aux;
+     aux = *pi1;
+    *pi1 = *pi2;
+    *pi2 = aux;
+}
+
+
+void ordenarEnteros(int viEnteros[], int iCantidad)
+{
+    int k=0, i;
+    while(k==0)
+    {
+        iCantidad-=1;
+        for(i=0 ; i < iCantidad; i++)
+        {
+            if (viEnteros[i] > viEnteros[i+1])
+                intercambio(&viEnteros[i], &viEnteros[i+1]);
+        }
+        if (iCantidad == 0)
+            k=1;
+
+    }
+}
