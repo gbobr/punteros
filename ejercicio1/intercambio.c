ab1186d91fd5609926ba655cae6ccd4d56012e57
AlejoMillo00 <43641714+AlejoMillo00@users.noreply.github.com>
2018-09-27 08:59:34 -0300

    Entrega Millo - Rinaldi 6°C Punteros Ejercicio 1

--- ejercicio1/intercambio.c
+++ ejercicio1/intercambio.c
@@ -1,3 +1,27 @@
 #include "intercambio.h"
 
-//Escriba aquí su código!
+void intercambio(int* C, int* D){
+
+    int aux;
+    aux = *C;
+    *C = *D;
+    *D = aux;
+
+}
+
+void ordenar_enteros(int viEnteros[], int iCantidad)
+{
+        int i, k = 1;
+        int aux;
+        while(k){
+            k = 0;
+            for(i = 0; i < iCantidad; i++)
+            {
+                if(viEnteros[i] > viEnteros[i+1]){
+                    intercambio(&viEnteros[i], &viEnteros[i+1]);
+                    k = 1;
+                }
+            }
+        }
+}
+
