f98b1a153b1a51cbbc359a78a3b259d0031fe67b
nahuel de bellis <debellisnahuel@gmail.com>
2018-10-12 09:26:15 -0300

    Add files via upload

--- ejercicio3/main.c
+++ ejercicio3/main.c
@@ -11,8 +11,9 @@ struct jugador {
 
 int main(int argc, char* argv[])
 {
-	struct jugador jugadores[MAX_PLAYERS];	
-	int i;
+    struct jugador* p[MAX_PLAYERS];
+	struct jugador jugadores[MAX_PLAYERS];
+	int i, ordenado = 1;
 
 	for(i=0; i < MAX_PLAYERS; i++) {
 		printf("Ingrese el nombre del jugador con id %d: ",i);
@@ -21,10 +22,27 @@ int main(int argc, char* argv[])
 		scanf("%d", &jugadores[i].puntaje);
 
 		jugadores[i].id=i;
+
+		p[i] = &jugadores[i];
 	}
 
 	printf("\n#SALIDA#\n");
-	
+    while(ordenado){
+        ordenado = 0;
+        for(i = 0; i < MAX_PLAYERS-1; i++){
+            if(p[i]->puntaje < p[i+1]->puntaje){
+                struct jugador* aux;
+                aux = p[i];
+                p[i]=p[i+1];
+                p[i+1]=aux;
+                ordenado = 1;
+            }
+        }
+    }
+    printf("Id, Nombre, Puntos");
+    for(i = 0;i < MAX_PLAYERS; i++){
+        printf("%d, %s, %d\n", p[i]->id, p[i]->nombre, p[i]->puntaje);
+    }
 //A PARTIR DE ESTE PUNTO LA SALIDA DEL PROGRAMA DEBE COINCIDIR EXACTAMENTE CON EL LOTE DE PRUEBA
 
 	return 0;
