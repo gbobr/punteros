60ef60de9b1a631b1dbe5e8f9b96c1f3a278fbcd
nahuel-de-bellis <debellisnahuel@gmail.com>
2018-10-15 17:21:03 -0300

    prueba fusionar branch

--- ejercicio1/intercambio.c
+++ ejercicio1/intercambio.c
@@ -2,20 +2,20 @@
 
 //Escriba aquí su código!
 void intercambio(int* p1, int* p2){
-	int aux;
-	aux = *p1;
+	int x;
+	x = *p1;
 	*p1 = *p2;
-	*p2 = aux; 
+	*p2 = x; 
 }
 
 void ordenarEnteros(int viEnteros[], int iCantidad){
-	int k = 1, i;
-	while(k){
-		k = 0;
+	int ordenado = 1, i;
+	while(ordenado){
+		ordenado = 0;
 		for(i = 0; i < iCantidad-1;i++){
 			if(viEnteros[i] > viEnteros[i+1]){
 				intercambio(&viEnteros[i], &viEnteros[i+1]);
-				k = 1;
+				ordenado = 1;
 			}
 		}
 	}
