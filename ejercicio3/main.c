7fadd3ae741d7a30c61ec56e5de80d80fbf5a446
Chuggen <43640215+Chuggen@users.noreply.github.com>
2018-10-18 07:57:20 -0300

    Ejercicio 3

--- ejercicio3/main.c
+++ ejercicio3/main.c
@@ -8,23 +8,42 @@ struct jugador {
 	int puntaje;
 	int id;
 };
-
+struct jugador *indice[MAX_PLAYERS];
 int main(int argc, char* argv[])
 {
 	struct jugador jugadores[MAX_PLAYERS];	
-	int i;
+	int i, aux;
 
-	for(i=0; i < MAX_PLAYERS; i++) {
+	for(i=0; i < MAX_PLAYERS; i++)
+		{
 		printf("Ingrese el nombre del jugador con id %d: ",i);
 		scanf("%s", jugadores[i].nombre);
 		printf("Ingrese el puntaje del jugador con id %d: ",i);
 		scanf("%d", &jugadores[i].puntaje);
 
 		jugadores[i].id=i;
-	}
+		}
+	
+	for(i=0;i<MAX_PLAYERS;i++)
+		{
+		 indice[i]=&jugadores[i];
+		}	
+	
+	for(i=0;i<MAX_PLAYERS;i++)
+	{
+		if(indice[i]->puntaje < indice[i+1]->puntaje)
+			{
+			aux= indice[i];
+			indice[i+1] = indice[i];
+			indice[i+1] = aux;
+			}}
 
 	printf("\n#SALIDA#\n");
-	
+	printf("Id \t Nombre \t Puntaje");
+		for(i=0;i<MAX_PLAYERS;i++)
+	{
+		printf("%d \t %c \t %d",indice[i]->id,indice[i]->nombre,indice[i]->puntaje);
+	}
 //A PARTIR DE ESTE PUNTO LA SALIDA DEL PROGRAMA DEBE COINCIDIR EXACTAMENTE CON EL LOTE DE PRUEBA
 
 	return 0;
