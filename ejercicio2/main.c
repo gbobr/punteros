d79cf76e664e1fc758fc747fc6be2288b8025b83
AlejoMillo00 <43641714+AlejoMillo00@users.noreply.github.com>
2018-10-18 09:27:07 -0300

    Add files via upload

--- ejercicio2/main.c
+++ ejercicio2/main.c
@@ -4,11 +4,26 @@
 int main(int argc, char* argv[])
 {
 
-//ESCRIBA AQUI ARRIBA EL CÃ“DIGO DE ENTRADA SALIDA PARA OBTENER LOS DATOS SOLICITADOS EN EL ENUNCIADO
+//ESCRIBA AQUI ARRIBA EL CÓDIGO DE ENTRADA SALIDA PARA OBTENER LOS DATOS SOLICITADOS EN EL ENUNCIADO
+    char lista[100];
+    char* elemento;
+    char* aux;
+    printf("Ingrese lista:\n");
+    gets(lista);
+    elemento = lista;
+
 
 	printf("\n#SALIDA#\n");
-	
+
 //A PARTIR DE ESTE PUNTO LA SALIDA DEL PROGRAMA DEBE COINCIDIR EXACTAMENTE CON EL LOTE DE PRUEBA
+        printf("%s\n", elemento);
+        for(elemento = lista; *elemento != '\0'; elemento++){
+            if(*elemento == ','){
+                aux = elemento;
+                aux = aux + 2;
+                printf("%s\n", aux);
+            }
+        }
 
 	return 0;
 }
