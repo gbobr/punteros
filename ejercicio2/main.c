7d33dd2fd54be0a91e228758bac3d8a42b5637de
mazzuca17 <43674685+mazzuca17@users.noreply.github.com>
2018-10-18 09:06:49 -0300

    Update main.c

--- ejercicio2/main.c
+++ ejercicio2/main.c
@@ -1,14 +1,23 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main(int argc, char* argv[])
+  int main(int argc, char* argv[])
 {
+ //ESCRIBA AQUI ARRIBA EL CÓDIGO DE ENTRADA SALIDA PARA OBTENER LOS DATOS SOLICITADOS EN EL ENUNCIADO
+ 	printf("\n#SALIDA#\n");
+     char elemento [100];
+    char* punt;
+    int punt_aux = 0;
 
-//ESCRIBA AQUI ARRIBA EL CÓDIGO DE ENTRADA SALIDA PARA OBTENER LOS DATOS SOLICITADOS EN EL ENUNCIADO
-
-	printf("\n#SALIDA#\n");
-	
-//A PARTIR DE ESTE PUNTO LA SALIDA DEL PROGRAMA DEBE COINCIDIR EXACTAMENTE CON EL LOTE DE PRUEBA
-
-	return 0;
+    gets(elemento);
+ //ESCRIBA AQUI ARRIBA EL CÓDIGO DE ENTRADA SALIDA PARA OBTENER LOS DATOS SOLICITADOS EN EL ENUNCIADO
+     for(punt = elemento;*punt != 0; punt++)
+    {
+        if(*punt == ','){
+            punt_aux = punt;
+            punt_aux = punt_aux+2;
+            printf("%s\n", punt_aux);
+        }
+    }
+  	return 0;
 }
