60484a85b6a6306d29e17c78bd52f63d4f5ede0b
mazzuca17 <43674685+mazzuca17@users.noreply.github.com>
2018-10-18 09:02:41 -0300

    Update main.c

--- ejercicio3/main.c
+++ ejercicio3/main.c
@@ -11,8 +11,9 @@ struct jugador {
 
 int main(int argc, char* argv[])
 {
-	struct jugador jugadores[MAX_PLAYERS];	
-	int i;
+	struct jugador jugadores[MAX_PLAYERS];
+	struct jugador * p[MAX_PLAYERS];
+	int i, aux, bandera = 0;
 
 	for(i=0; i < MAX_PLAYERS; i++) {
 		printf("Ingrese el nombre del jugador con id %d: ",i);
@@ -23,9 +24,41 @@ int main(int argc, char* argv[])
 		jugadores[i].id=i;
 	}
 
+	for(i=0; i<MAX_PLAYERS;i++){
+
+         p[i] = &jugadores[i];
+	}
+
 	printf("\n#SALIDA#\n");
-	
+
+
+	while(!bandera)
+    {
+
+        bandera=1;
+
+        for(i=0; i< MAX_PLAYERS-1; i++)
+        {
+            if(p[i]->puntaje < p[i+1]->puntaje)
+            {
+                bandera = 0;
+                aux=p[i];
+                p[i]=p[i+1];
+                p[i+1] = aux;
+            }
+        }
 //A PARTIR DE ESTE PUNTO LA SALIDA DEL PROGRAMA DEBE COINCIDIR EXACTAMENTE CON EL LOTE DE PRUEBA
+
+
+    }
+    printf("\nID,  NOMBRE,  PUNTAJE");
+
+        for(i=0; i<MAX_PLAYERS; i++){
+
+            printf("\n%d,      %s,      %d", p[i]->id, p[i]->nombre, p[i]->puntaje);
+        }
+
+        printf("\nCreated by: Mazzuca Matias and Surace De Mattei Lucas");
 
 	return 0;
 }
