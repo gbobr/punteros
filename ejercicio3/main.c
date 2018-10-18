1a0c5560a373b8a997d9c9f49aeb36e8299c1750
OldSnake256 <43642167+OldSnake256@users.noreply.github.com>
2018-10-18 08:44:34 -0300

    celestino 6°C
    skere

--- ejercicio3/main.c
+++ ejercicio3/main.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#define MAX_PLAYERS 4
+#define MAX_PLAYERS 2
 
 struct jugador {
 	char nombre[32];
@@ -11,20 +11,45 @@ struct jugador {
 
 int main(int argc, char* argv[])
 {
-	struct jugador jugadores[MAX_PLAYERS];	
-	int i;
+    struct jugador* punteros [MAX_PLAYERS];
+	struct jugador jugadores[MAX_PLAYERS];
+	int i, ordenado=1;
+	struct jugador *aux;
 
 	for(i=0; i < MAX_PLAYERS; i++) {
 		printf("Ingrese el nombre del jugador con id %d: ",i);
 		scanf("%s", jugadores[i].nombre);
 		printf("Ingrese el puntaje del jugador con id %d: ",i);
 		scanf("%d", &jugadores[i].puntaje);
-
+        punteros[i] = &jugadores [i];
 		jugadores[i].id=i;
 	}
 
+	while(ordenado){
+        ordenado = 0;
+        for (i=0; i < MAX_PLAYERS-1; i++)
+        {
+            if (punteros[i]->puntaje < punteros[i+1]->puntaje)
+            {
+                aux = punteros[i+1];
+                punteros[i+1] = punteros[i];
+                punteros[i] = aux;
+
+                ordenado = 1;
+            }
+        }
+	}
+
 	printf("\n#SALIDA#\n");
-	
+
+	for (i=0; i < MAX_PLAYERS; i++)
+    {
+        printf("%d, %s, %d\n", punteros[i]->id, punteros[i]->nombre, punteros[i]->puntaje);
+
+    }
+
+
+
 //A PARTIR DE ESTE PUNTO LA SALIDA DEL PROGRAMA DEBE COINCIDIR EXACTAMENTE CON EL LOTE DE PRUEBA
 
 	return 0;
