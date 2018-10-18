3a2eaac0e9963dbde81f514876ae3521a8223839
toripampa <43849533+toripampa@users.noreply.github.com>
2018-10-18 09:17:37 -0300

    Add files via upload

--- ejercicio2/main.c
+++ ejercicio2/main.c
@@ -3,11 +3,28 @@
 
 int main(int argc, char* argv[])
 {
+     char* p;
+     char palabras [100];
+     gets(palabras);
+     p = palabras;
+     int i;
+
+
 
 //ESCRIBA AQUI ARRIBA EL CÓDIGO DE ENTRADA SALIDA PARA OBTENER LOS DATOS SOLICITADOS EN EL ENUNCIADO
 
 	printf("\n#SALIDA#\n");
-	
+
+    printf("%s\n", p);
+
+     for(p = palabras; *p != 0; p++)
+        {
+         if(*p == ','){
+            printf("%s\n", p+2);
+         }
+     }
+
+
 //A PARTIR DE ESTE PUNTO LA SALIDA DEL PROGRAMA DEBE COINCIDIR EXACTAMENTE CON EL LOTE DE PRUEBA
 
 	return 0;
