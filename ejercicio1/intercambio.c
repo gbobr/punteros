2e1655ddd9e240458f1f898e244d4a3ab7ba5aab
Portaliekx <44263591+Portaliekx@users.noreply.github.com>
2018-10-25 09:09:31 -0300

    Add files via upload

--- ejercicio1/intercambio.c
+++ ejercicio1/intercambio.c
@@ -1,3 +1,27 @@
 #include "intercambio.h"
 
-//Escriba aquí su código!
+void intercambio(int* n2, int* n1){
+
+    int aux;
+    aux = *n2;
+    *n2 = *n1;
+    *n1 = aux;
+}
+
+void ordenarEnteros(int viEnteros[], int iCantidad){
+    int aux, band=0, i;
+
+    while(!band){
+        band=1;
+        for(i=0;i<iCantidad;i++){
+            if(viEnteros[i]>viEnteros[i+1]){
+                aux=viEnteros[i];
+                viEnteros[i]=viEnteros[i+1];
+                viEnteros[i+1]=aux;
+                band=0;
+            }
+
+        }
+
+    }
+}
